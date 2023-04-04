8cc247c80215e75feafac1a50f09a9f3370f5a48
VPivotto <68520601+VPivotto@users.noreply.github.com>
2023-04-04 13:25:10 -0300

    Update script.js

--- dataviz_2/script.js
+++ dataviz_2/script.js
@@ -1,4 +1,4 @@
-d3.csv('/astronautas.csv', d3.autoType).then(data => {   
+d3.csv('../astronautas.csv', d3.autoType).then(data => {   
   let chart = Plot.plot({     
     marks: [       
     Plot.dot(data, Plot.groupX({y:"mean"},{         
